cd3f6d16663f63443ff428f07d38a3a8fc5f08f0
浅見 朋 Asami Tomo <luminescence_33@icloud.com>
2017-05-01 20:04:52 +0900

    リポジトリ及びプロジェクトの再作成

---
+++ 時間割/時間割/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  時間割
+//
+//  Created by 浅見朋 on 2017/05/01.
+//  Copyright © 2017年 浅見朋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

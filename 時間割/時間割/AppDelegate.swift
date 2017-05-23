91715c43aa6f29bfe65439911125ff0a3ff2902b
沼田悠貴 <chachamalun@gmail.com>
2017-05-23 14:56:26 +0900

    テーブルの変更

--- 時間割/時間割/AppDelegate.swift
+++ 時間割/時間割/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var tag:Int = -1
 
     /*func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ 時間割/時間割/Setting.swift
@@ -0,0 +1,44 @@
+//
+//  Setting.swift
+//  時間割
+//
+//  Created by 浅見朋 on 2017/05/22.
+//  Copyright © 2017年 浅見朋. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Seting: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 設定されたタイトルを代入するとナビゲーションコントローラに反映される。
+        self.title = "設定"
+        // 背景色
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //ボタンが押された時の処理。
+    internal func onClickMyButton(sender: UIButton) {
+        print("onClickMyButton:");
+        print("sender.tag: \(sender.tag)")
+        
+        
+        // 遷移するViewを定義する.
+        let mySecondViewController: UIViewController = One_VC()
+        // Viewの移動する.
+        self.navigationController?.pushViewController(mySecondViewController, animated: true)
+    }
+    
+}
+

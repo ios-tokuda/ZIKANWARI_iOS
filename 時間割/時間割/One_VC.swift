4f578170b5d97eaa94f2351d53eb91010b057622
沼田悠貴 <chachamalun@gmail.com>
2017-05-26 16:54:40 +0900

    一コマをセクション付きで表示。ナビゲーションボタンの追加

--- 時間割/時間割/One_VC.swift
+++ 時間割/時間割/One_VC.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // Tableで使用する配列を設定する
-    private let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
-    private var myTableView: UITableView!
+    // Tableで使用する配列を定義する.
+    var className: NSArray = ["331"]            //クラスの場所
+    var teachName: NSArray = ["我妻"]             //講師の名前
+    var timeName: NSArray = ["16:40~"]          //授業時間
+    var kadai: NSArray = []                     //課題を入れていく
+    //private let myiPhoneItems: NSArray = ["iOS9","iOS8", "iOS7", "iOS6", "iOS5", "iOS4"]
+    //private let myAndroidItems: NSArray = ["5.x", "4.x", "2.x", "1.x"]
+    
+    
+    // Sectionで使用する配列を定義する.
+    private let mySections: NSArray = ["教室", "講師", "時間", "課題"]
+
     
     //deligateにおいてあるメンバにはここからアクセス
     //VCをまたいで値を渡したい時などに用いる
@@ -23,36 +32,57 @@ class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tag:Int = -1
     
+    //ナビゲーションボタンの生成
+    private var myRightButton: UIBarButtonItem!
+    
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tag = self.delegate.tag
+                print("sender.tag: \(self.tag)")
         
-        //ナビゲーションコントーラーに反映
+        
+        //NavigationControllerのタイトルを設定する
         self.title = "講義名"
         
+        //右ボタンを作成する
+        myRightButton = UIBarButtonItem(title: "編集", style: .plain, target: self, action: #selector(Week_VC.onClickMyButton(sender:)))
+        
+        //ナビゲーションバーの右に設置する
+        self.navigationItem.rightBarButtonItem = myRightButton
+        
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+
         // 背景色
         self.view.backgroundColor = self.delegate.BGColor
+        
+        // Status Barの高さを取得をする.
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
         
         // Viewの高さと幅を取得する.
         let displayWidth: CGFloat = self.view.frame.width
         let displayHeight: CGFloat = self.view.frame.height
         
-        // TableViewの生成
-        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
+        // TableViewの生成( status barの高さ分ずらして表示 ).
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
         
         // Cell名の登録をおこなう.
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
         
-        // DataSourceを自身に設定する.
+        // DataSourceの設定をする.
         myTableView.dataSource = self
         
-        // Delegateを自身に設定する.
+        // Delegateを設定する.
         myTableView.delegate = self
         
         // Viewに追加する.
         self.view.addSubview(myTableView)
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -60,30 +90,78 @@ class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
+    
     /*
-     Cellが選択された際に呼び出される
+     ボタンイベント.
+     */
+    internal func onClickMyButton(sender: UIButton){
+            self.view.backgroundColor = UIColor.red
+    }
+
+    
+    /*
+     セクションの数を返す.
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return mySections.count
+    }
+    
+    /*
+     セクションのタイトルを返す.
+     */
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return mySections[section] as? String
+    }
+    
+    /*
+     Cellが選択された際に呼び出される.
      */
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Num: \(indexPath.row)")
-        print("Value: \(myItems[indexPath.row])")
+        if indexPath.section == 0 {
+            print("Value: \(className[indexPath.row])")
+        } else if indexPath.section == 1 {
+            print("Value: \(teachName[indexPath.row])")
+        } else if indexPath.section == 2 {
+            print("Value: \(timeName[indexPath.row])")
+        } else if indexPath.section == 3 {
+            print("Value: \(kadai[indexPath.row])")
+        }
     }
     
     /*
-     Cellの総数を返す.
+     テーブルに表示する配列の総数を返す.
      */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myItems.count
+        if section == 0 {
+            return className.count
+        } else if section == 1 {
+            return teachName.count
+        } else if section == 2 {
+            return timeName.count
+        } else if section == 3
+        {
+            return kadai.count
+        }else {
+            return 0
+        }
     }
     
     /*
-     Cellに値を設定する
+     Cellに値を設定する.
      */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 再利用するCellを取得する.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
         
-        // Cellに値を設定する.
-        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = "\(className[indexPath.row])"
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = "\(teachName[indexPath.row])"
+        } else if indexPath.section == 2 {
+            cell.textLabel?.text = "\(timeName[indexPath.row])"
+        } else if indexPath.section == 3 {
+            cell.textLabel?.text = "\(kadai[indexPath.row])"
+        }
         
         return cell
     }

d4e2e635d9eed43a78ddbf65fe3ecd4ad6e04f3f
Asami Tomo <luminescence_33@icloud.com>
2017-06-19 16:04:01 +0900

    修正中

--- 時間割/時間割/One_VC.swift
+++ 時間割/時間割/One_VC.swift
@@ -10,22 +10,32 @@ import UIKit
 
 class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // Tableで使用する配列を定義する.
-    var className: Array = ["331"]            //クラスの場所
-    var teachName: Array = ["我妻"]             //講師の名前
-    var timeName: Array = ["16:40~"]          //授業時間
-    var taskName: [String] = []                  //課題を入れていく
+    // Tableで使用する配列を設定する
+    var taskName: [String] = []
+    var myTableView: UITableView!
+    let imageNames = ["door2.jpg", "clock2.jpg", "hito2.jpg"]
+    
+    //セルに表示するテキストをいじるときのやつ
+    let tempS = "おまけ"
+    
+    //課題が達成したかどうかの判別(最終的にはHomeWorkクラス作成)
+    var finished = false
+    
+    var deadline: [String] = ["aaa"]             //
     
     var taskN = "!!!"
     
-    //private let myiPhoneItems: NSArray = ["iOS9","iOS8", "iOS7", "iOS6", "iOS5", "iOS4"]
-    //private let myAndroidItems: NSArray = ["5.x", "4.x", "2.x", "1.x"]
+    //バグ(1)セルが無いときにセルを追加するとずれる
+    //バグ(1)解決のための変数
+    var taskExist = false
     
     
-    // Sectionで使用する配列を定義する.
-    private let mySections: NSArray = ["教室", "講師", "時間", "課題"]
-
+    // Status Barの高さを取得をする.
+    var barHeight: CGFloat!
     
+    // Viewの高さと幅を取得する.
+    var displayWidth: CGFloat!
+    var displayHeight: CGFloat!
     
     
     var inputKadai: UITextView!
@@ -55,41 +65,26 @@ class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         //NavigationControllerのタイトルを設定する
         self.title = "講義名"
         
+        // Status Barの高さを取得をする.
+        barHeight = UIApplication.shared.statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する.
+        displayWidth = self.view.frame.width
+        displayHeight = self.view.frame.height
+
+        
+        
         //右ボタンを作成する
         myRightButton = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(One_VC.onClickMyButton(sender:)))
         
-        //ナビゲー１ションバーの右に設置する
+        //ナビゲーションバーの右に設置する
         self.navigationItem.rightBarButtonItem = myRightButton
         
         
-        self.view.backgroundColor = UIColor.red
+        self.createSection()
         
-        
+        self.createDisplay()
 
-        // 背景色
-        self.view.backgroundColor = self.delegate.BGColor
-        
-        // Status Barの高さを取得をする.
-        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
-        
-        // Viewの高さと幅を取得する.
-        let displayWidth: CGFloat = self.view.frame.width
-        let displayHeight: CGFloat = self.view.frame.height
-        
-        // TableViewの生成( status barの高さ分ずらして表示 ).
-        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
-        
-        // Cell名の登録をおこなう.
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-        
-        // DataSourceの設定をする.
-        myTableView.dataSource = self
-        
-        // Delegateを設定する.
-        myTableView.delegate = self
-        
-        // Viewに追加する.
-        self.view.addSubview(myTableView)
     }
     
     override func didReceiveMemoryWarning() {
@@ -98,90 +93,186 @@ class One_VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     
-    /*
-     +ボタンが押された時のイベント
-     */
+    
+    //+ボタンが押された時のイベント
     internal func onClickMyButton(sender: UIButton){
-        let showInput = AlertInput().OneInput(one: self)
+        AlertInput().OneInput(one: self)
         
         //self.taskN = self.delegate.taskName
         print(taskN)
-        print(taskN)
-        print(taskN)
-        print(taskN)
+        
         //self.taskName.append(self.delegate.taskName)
-        self.view.backgroundColor = UIColor.red
     }
 
-    
-    /*
-     セクションの数を返す.
-     */
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return mySections.count
+    //課題以外の表示部分の作成
+    func createDisplay()
+    {
+        Frame()
+        
+        Icon()
+        
+        cLabel()
+        
     }
     
-    /*
-     セクションのタイトルを返す.
-     */
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return mySections[section] as? String
+    //外枠より少し小さい内枠をあとからviewに追加することによって枠を作る
+    func Frame()
+    {
+        
+        //外枠と内枠の作成
+        let frame : UIView = UIView(frame: CGRect(x: 0, y: barHeight+50, width: displayWidth, height: 100))
+        let innerFrame : UIView = UIView(frame: CGRect(x: 5, y: barHeight+50, width: displayWidth-10, height: 95))
+        
+        //外枠と内枠の色の指定
+        frame.backgroundColor = self.delegate.BGColor
+        innerFrame.backgroundColor = UIColor.white
+        
+        //外枠と内枠をviewに追加する
+        self.view.addSubview(frame)
+        self.view.addSubview(innerFrame)
+
     }
     
-    /*
-     Cellが選択された際に呼び出される.
-     */
+    //アイコンの表示
+    func Icon()
+    {
+        // 講師用、教室用、授業時間用の画像をそれぞれ設定する.
+        let hitoIcon: UIImage = UIImage(named: "hito2.jpg")!
+        let doorIcon: UIImage = UIImage(named: "door2.jpg")!
+        let clockIcon: UIImage = UIImage(named: "clock2.jpg")!
+        
+        //各アイコンの大きさ
+        let imageWidth: CGFloat = 50
+        let imageHeight: CGFloat = 50
+        
+        // それぞれのアイコンの表示用のUIImageViewを生成.
+        let hitoDownView: UIImageView = UIImageView(frame:  CGRect(x: 10, y: barHeight+50, width: imageWidth, height: imageHeight))
+        let doorDownView: UIImageView = UIImageView(frame: CGRect(x: 200, y: barHeight+50, width: imageWidth, height: imageHeight))
+        let clockDownView: UIImageView = UIImageView(frame: CGRect(x: 10, y: barHeight+95, width: imageWidth, height:imageHeight))
+        
+        // UIImageViewにそれぞれのアイコンの画像を設定する.
+        hitoDownView.image = hitoIcon
+        doorDownView.image = doorIcon
+        clockDownView.image = clockIcon
+        
+        // 縮小用(0.5倍)のアフィン行列を生成する.
+        hitoDownView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        doorDownView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        clockDownView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        
+        // Viewに追加する.
+        self.view.addSubview(hitoDownView)
+        self.view.addSubview(doorDownView)
+        self.view.addSubview(clockDownView)
+
+    }
+    
+    //ラベルとタイトルの表示
+    func cLabel()
+    {
+        
+        //NavigationControllerのタイトルを設定する
+        self.title = "講義名"
+        
+        // 各項目のLabelを作成.
+        let hitoLabel: UILabel = UILabel(frame: CGRect(x: 55, y: barHeight+50, width: 200, height: 50))
+        let doorLabel: UILabel = UILabel(frame: CGRect(x: 245, y: barHeight+50, width: 200, height: 50))
+        let clockLabel: UILabel = UILabel(frame: CGRect(x: 55, y: barHeight+95, width: 200, height: 50))
+        
+        
+        hitoLabel.text = "ところ天の助"
+        doorLabel.text = "222"
+        clockLabel.text = "9:30 ~ 11:00"
+        
+        // ViewにLabelを追加.
+        self.view.addSubview(hitoLabel)
+        self.view.addSubview(doorLabel)
+        self.view.addSubview(clockLabel)
+
+    }
+    
+    //課題セクションを作成(課題の変更を更新)
+    func createSection()
+    {
+        // 背景色
+        self.view.backgroundColor = self.delegate.BGColor
+        
+        // TableViewの生成(Status barの高さをずらして表示).
+        
+        if taskExist == false{
+            myTableView = UITableView(frame: CGRect(x: 0, y: barHeight + 100, width: displayWidth, height: displayHeight))
+        }else{
+            myTableView = UITableView(frame: CGRect(x: 0, y: barHeight + 150, width: displayWidth, height: displayHeight))
+        }
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.imageView?.image = UIImage(named: "hito2.jpg")!
+        
+        // Cell名の登録をおこなう.
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceを自身に設定する.
+        myTableView.dataSource = self
+        
+        // Delegateを自身に設定する.
+        myTableView.delegate = self
+        
+        // Viewに追加する.
+        self.view.addSubview(myTableView)
+
+    }
+    
+    
+    
+    //Cellが選択された際に呼び出される
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 0 {
-            print("Value: \(className[indexPath.row])")
-        } else if indexPath.section == 1 {
-            print("Value: \(teachName[indexPath.row])")
-        } else if indexPath.section == 2 {
-            print("Value: \(timeName[indexPath.row])")
-        } else if indexPath.section == 3 {
-            print("Value: \(taskName[indexPath.row])")
+        if finished{
+            self.finished = false
+        }else{
+            self.finished = true
         }
+        self.createSection()
+        print("Num: \(indexPath.row)")
+        print("Value: \(taskName[indexPath.row])")
     }
     
-    /*
-     テーブルに表示する配列の総数を返す.
-     */
+    
+    //Cellの総数を返す.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return className.count
-        } else if section == 1 {
-            return teachName.count
-        } else if section == 2 {
-            return timeName.count
-        } else if section == 3
-        {
-            return taskName.count
-        }else {
-            return 0
-        }
+        print("CellCount: \(taskName.count)")
+        return taskName.count
     }
     
-    /*
-     Cellに値を設定する.
-     */
+    
+    //Cellに値を設定する
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        
-        if indexPath.section == 0 {
-            cell.textLabel?.text = "\(className[indexPath.row])"
-        } else if indexPath.section == 1 {
-            cell.textLabel?.text = "\(teachName[indexPath.row])"
-        } else if indexPath.section == 2 {
-            cell.textLabel?.text = "\(timeName[indexPath.row])"
-        } else if indexPath.section == 3 {
-            cell.textLabel?.text = "\(taskName[indexPath.row])"
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        if !self.finished{
+            cell.imageView?.image = UIImage(named: "hito2.jpg")!
+        }else{
+            cell.imageView?.image = UIImage(named: "door2.jpg")!
         }
+        // 再利用するCellを取得する.
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(taskName[indexPath.row])"
+        cell.detailTextLabel?.text = "10/9   9:10" // 期限
         
         return cell
     }
     
+    func sendPickTime()
+    {
+        
+        
+        // 遷移するViewを定義する.
+        let picktime: UIViewController = PickTime()
+        // Viewの移動する.
+        self.navigationController?.pushViewController(picktime, animated: true)
+        
+
+    }
+    
 }
-
-
-
